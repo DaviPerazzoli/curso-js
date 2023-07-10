a3683e037bea45a11f4101d64218d4e0c7f06f52
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-07-10 14:11:16 -0300

    comecei a aprender condições

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 80){
+    console.log("Você ultrapassou o limite! MULTADO!")
+}else{
+    console.log("Lembre do cinto de segurança")
+}
