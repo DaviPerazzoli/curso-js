9565136a065aca6263e1abb24d58d1aec7e0ae46
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-07-12 12:48:32 -0300

    mudei pra let

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,12 +1,12 @@
 function tabuada(){
-    var num = document.getElementById('inum').value
-    var tab = document.getElementById('itab')
+    let num = document.getElementById('inum').value
+    let tab = document.getElementById('itab')
 
     if(num.length == 0){
         alert('Preencha o campo corretamente!')
     }else{
         tab.innerHTML = ''
-        for(c = 1; c <= 10; c++){
+        for(let c = 1; c <= 10; c++){
             tab.innerHTML += `<option>${num} x ${c} = ${num * c}</option>`
         }
     }
