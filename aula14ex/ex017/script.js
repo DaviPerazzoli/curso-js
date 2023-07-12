b10c51d1449053d96581f433c792fa0c903df36d
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-07-12 12:56:13 -0300

    atualizei o ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -7,7 +7,10 @@ function tabuada(){
     }else{
         tab.innerHTML = ''
         for(let c = 1; c <= 10; c++){
-            tab.innerHTML += `<option>${num} x ${c} = ${num * c}</option>`
+            //tab.innerHTML += `<option>${num} x ${c} = ${num * c}</option>`
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num * c}`
+            tab.appendChild(item)
         }
     }
 }
