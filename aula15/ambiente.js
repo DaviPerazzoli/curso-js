3051d375fac358adaae0c19d28589c89af5e622b
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-07-13 11:20:17 -0300

    aprendi vetores em js

---
+++ aula15/ambiente.js
@@ -0,0 +1,20 @@
+let vaga = ['carro1', 'carro2', 'carro3']
+let num = [1,5,2,3,9,5]
+/*
+console.log(num.length)
+console.log(num)
+num.sort()
+num.push(5)
+num.push(90)
+console.log(num)
+
+for(let i = 0; i < num.length; i++){
+    console.log(num[i])
+}
+*/
+
+for(let i in num){
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+}
+
+console.log(num.indexOf(9))
