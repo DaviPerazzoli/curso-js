19726acc45dedb26fcaf8ca0a11075c7b586fa64
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-07-13 11:29:03 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,16 +1,16 @@
 let vaga = ['carro1', 'carro2', 'carro3']
 let num = [1,5,2,3,9,5]
 /*
-console.log(num.length)
-console.log(num)
-num.sort()
-num.push(5)
-num.push(90)
-console.log(num)
-
-for(let i = 0; i < num.length; i++){
-    console.log(num[i])
-}
+* console.log(num.length)
+* console.log(num)
+* num.sort()
+* num.push(5)
+* num.push(90)
+* console.log(num)
+* 
+* for(let i = 0; i < num.length; i++){
+*     console.log(num[i])
+* }
 */
 
 for(let i in num){
