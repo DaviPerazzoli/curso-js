7f5d69cd63ab5ab5d7a5bd97c3453ee13edc812e
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-08-28 20:37:50 -0300

    for in e of

---
+++ curso2/16/aula16.js
@@ -0,0 +1,22 @@
+let num = [10, 20, 30, 40, 50]
+
+/* 
+* for(let i=0;i<num.length;i++){
+*     document.writeln(num[i])
+* }
+*/
+
+const objs = document.getElementsByTagName('div')
+
+for(i in num){
+    console.log(num[i])
+}
+
+for(i of num){
+    console.log(i)
+}
+
+for(o of objs){
+    o.innerText = 'ola'
+}
+
