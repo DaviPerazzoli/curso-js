16de1f52c7c773fdf7b394acda8062a9bf2f0bfa
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-08-31 20:47:36 -0300

    solucionei a pratica 1

--- curso2/35/pratica1.js
+++ curso2/35/pratica1.js
@@ -1,18 +1,30 @@
+const itens = [...document.getElementsByClassName('curso')]
+const botao = document.getElementById('btnCopiar')
+const caixa2 = document.getElementById('caixa2')
+const caixa1 = document.getElementById('caixa1')
+
 const selecionar = (elemento)=>{
-    elemento.target.classList.add('destaque')
+    elemento.target.classList.toggle('destaque')
 }
 
 const copiar = ()=>{
-    selecionados = [...document.getElementsByClassName('destaque')]
-    caixa2 = document.getElementById('caixa2')
+    const selecionados = [...document.getElementsByClassName('destaque')]
+    itens.map((el)=>{
+        const classes = [...el.classList]
+        let selecionado = false
+        classes.map((el)=>{
+            if(el == 'destaque'){
+                selecionado = true
+            }
+        })
+        if(!selecionado){
+            caixa1.appendChild(el)
+        }
+    })
     selecionados.map((el)=>{
         caixa2.appendChild(el)
     })
-   // caixa2.appendChild(selecionados)
 }
-
-itens = [...document.getElementsByClassName('curso')]
-botao = document.getElementById('btnCopiar')
 
 itens.map((el)=>{
     el.addEventListener('click', selecionar)
