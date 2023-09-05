ed75ec5a08ae154d404e2fa93f1f0d543f6a1785
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-09-05 19:29:11 -0300

    aprendi sobre Set

---
+++ curso2/56/set.js
@@ -0,0 +1,18 @@
+const caixa =document.getElementById('caixa')
+
+let musicas= new Set(["musica1", "musica boa", "musica10"])
+
+musicas.add("musica muito legal")
+musicas.add("musica1")     //* São ignoradas
+musicas.add("musica1")
+musicas.add("musica10")
+
+console.log(musicas)
+
+// musicas.forEach((el)=>{
+//     caixa.innerHTML+=el+"<br>"
+// })
+
+for(let m of musicas){
+    caixa.innerHTML+=m+"<br>"
+}
