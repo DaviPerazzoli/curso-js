19066aafe21effb4f989022fde63fff39ddf77ff
DaviPerazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2023-09-05 19:49:16 -0300

    pratiquei template strings

---
+++ curso2/56/templateStrings.js
@@ -0,0 +1,7 @@
+const caixa =document.getElementById('caixa')
+
+const curso='Javascript'
+const canal='CFB cursos'
+const frase=`Este é o curso de ${curso} do canal ${canal}`
+
+caixa.innerHTML=frase
