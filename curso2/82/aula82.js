f64011fd93a8f7196c27999f639c8e3bfabf2091
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-05 23:16:56 -0300

    Update aula82.js

--- curso2/82/aula82.js
+++ curso2/82/aula82.js
@@ -9,3 +9,33 @@ let alturaPalco=palco.offsetHeight
 let bolas=[]
 let numBolas = 0
 
+class Bola{
+    constructor(){
+        this.tam=Math.floor(Math.random()*10)+10
+        this.r = Math.floor(Math.random()*255)
+        this.g = Math.floor(Math.random()*255)
+        this.b = Math.floor(Math.random()*255)
+        this.px=Math.floor(Math.random()*(larguraPalco-this.tam))
+        this.py=Math.floor(Math.random()*(alturaPalco-this.tam))
+    }
+
+    
+}
+
+window.addEventListener('resize', (e)=>{
+    let larguraPalco=palco.offsetWidth
+    let alturaPalco=palco.offsetHeight
+})
+
+btn_add.addEventListener('click', (e)=>{
+    const qtde = txt_qtde.ariaValueMax
+    for(let i=0; i< qtde; i++){
+        //instanciar novas bolinhas e colocar no array bolas
+    }
+})
+
+btn_remover.addEventListener('click', (e)=>{
+    bolas.map((b)=>{
+        //remover bolinha
+    })
+})
