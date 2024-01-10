20c250b0c26440b1c4706bdbb4b54bc446672ba4
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-09 21:26:15 -0300

    aprendi mais sobre objetos literais

--- curso2/90/objLiterais.js
+++ curso2/90/objLiterais.js
@@ -1,18 +1,35 @@
 const objetos = document.getElementById('objetos')
 
 let computador = {
-    cpu: 'i9',
-    ram: '64gb',
-    hd: '2tb',
+    cpu: '',
+    ram: '',
+    hd: '',
     info: function(){
         console.log(`CPU:${this.cpu}`)
         console.log(`RAM:${this.ram}`)
         console.log(`HD:${this.hd}`)
     }
 }
+// computador['monitor']='22pol'
+// computador.placaVideo='rtx'
 
-computador['monitor']='22pol'
-computador.placaVideo='rtx'
+const c1 = Object.assign({},computador)
+
+delete c1.hd
+
+// c1.info()
+
+const o1 ={obj1: '1'}
+const o2 ={obj2: '2'}
+const o3 ={obj3: '3'}
+
+const o4 = Object.assign(o1,o2,o3)
+
+console.log(o4)
+
+const c2 = Object.create(computador)
+c2.cpu = 'amd'
+c2.info()
 
 const computadores=[{
         cpu: 'i9',
@@ -30,7 +47,7 @@ const computadores=[{
         hd: '1tb'
     }]
 
-computador.info()
+// computador.info()
 
 // objetos.innerHTML= JSON.stringify(computadores)
 
