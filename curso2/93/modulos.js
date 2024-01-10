afe180787c4aeab3e48760d4a9ed9b49173977e1
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-10 18:20:29 -0300

    aprendendo sobre módulos em js

---
+++ curso2/93/modulos.js
@@ -0,0 +1,4 @@
+import getTodosCursos, {cursos2, getCurso} from '../cursos2.js'
+
+console.log(getTodosCursos())
+console.log(getCurso(2))
---
+++ curso2/cursos2.js
@@ -0,0 +1,10 @@
+export const cursos2 = ['Javascript', 'HTML', 'CSS', 'Arduino', 'Raspberry', 'C++', 'Python', 'Java', 'C#']
+
+export default function getTodosCursos(){
+    return cursos2
+}
+
+export function getCurso(index){
+    return cursos2[index]
+}
+
