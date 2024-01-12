bc22ac1b195a31963bf03caefbd9b441bf6f9c01
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-12 20:36:53 -0300

    comecei a aprender manipulação de strings

---
+++ curso2/101/string.js
@@ -0,0 +1,12 @@
+let nome = 'bruno pinho campos'
+let canal = 'CFB cursos'
+
+console.log(nome)
+
+console.log(nome.charAt(1))
+console.log(nome.charCodeAt(1))
+
+console.log(nome.concat(canal))
+console.log(nome.indexOf('o'))
+
+console.log(nome.lastIndexOf('o'))
