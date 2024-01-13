055a16e18a88a5b5254b952f30c44da541dc2a2f
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-12 23:52:14 -0300

    aprendi os métodos mais recentes para strings

--- curso2/104/string.js
+++ curso2/104/string.js
@@ -4,3 +4,10 @@ console.log(nome.startsWith('pinho', 6))
 console.log(nome.endsWith('campos'))
 
 console.log(nome.includes('nh'))
+
+console.log(nome.repeat(4))
+
+console.log(nome)
+
+console.log(String.fromCharCode(nome.charCodeAt(0),nome.charCodeAt(1),nome.charCodeAt(2),nome.charCodeAt(3),nome.charCodeAt(4)))
+
