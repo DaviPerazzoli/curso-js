40d90c38d1e8c3c8338173d303dc9dd91019c37c
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-12 22:25:32 -0300

    aprendendo mais sobre strings

--- curso2/101/string.js
+++ curso2/101/string.js
@@ -1,4 +1,5 @@
-let nome = 'bruno pinho campos'
+let nome = 'bruno Pinho campos'
+let nome2 = 'bruno Pinho campos'
 let canal = 'CFB cursos'
 
 console.log(nome)
@@ -10,3 +11,24 @@ console.log(nome.concat(canal))
 console.log(nome.indexOf('o'))
 
 console.log(nome.lastIndexOf('o'))
+console.log(nome.localeCompare(nome2))
+
+console.log(nome.replace('Pinho', 'Teste'))
+
+console.log(nome.search('Pinho'))
+
+console.log(nome.slice(6,9))
+
+let arr_nome = nome.split(' ')
+console.log(arr_nome)
+
+let parte=nome.substring(0,5)
+console.log(parte)
+
+console.log(nome.toUpperCase())
+console.log(nome.toLowerCase())
+
+console.log(nome.valueOf())
+
+let num=10
+console.log(typeof(num.toString()))
