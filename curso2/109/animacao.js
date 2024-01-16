ef582cc721ca7b065dd69c3c78403db6008a9826
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-15 22:52:21 -0300

    atualizei o exercício de animação

--- curso2/109/animacao.js
+++ curso2/109/animacao.js
@@ -3,47 +3,37 @@ const btn_parar = document.getElementById('btn_parar')
 const btn_esquerda = document.getElementById('btn_esquerda')
 const btn_direita = document.getElementById('btn_direita')
 
-let tamMax = window.innerWidth - parseInt(carro.style.width)
-let anima=null
-
 const init = ()=>{
     carro.style.position='relative'
     carro.style.left = '0px'
-    // carro.style.transition= '0.1s left'
-    carro.style.width = '100px'
-    redefinirTamMax()
+    carro.style.transition= '0.1s left'
 }
 
-const redefinirTamMax=()=>{
-    tamMax = window.innerWidth - parseInt(carro.style.width)
+const posicaoDentroDaTela = ()=>{
+    return parseInt(carro.style.left) >= 0 && parseInt(carro.style.left) < (document.body.offsetWidth - carro.offsetWidth)
 }
 
-//* utilizando recursividade
+let anima=null
+
 const mover=(dir)=>{
-    console.log(tamMax)
     let posicao_carro = parseInt(carro.style.left)
-    if(posicao_carro > tamMax || posicao_carro < 0){
-        dir *= -1
-    }
     posicao_carro+= 10 * dir
     carro.style.left = posicao_carro + 'px'
-    anima=setTimeout(mover, 20, dir)
 }
 
 btn_parar.addEventListener('click', ()=>{
-    clearTimeout(anima)
+    clearInterval(anima)
 })
 
 btn_esquerda.addEventListener('click', (evt)=>{
-    clearTimeout(anima)
-    mover(-1)
+    clearInterval(anima)
+    anima = setInterval(mover, 20, -1)
 })
 
 btn_direita.addEventListener('click', ()=>{
-    clearTimeout(anima)
-    mover(1)
+    clearInterval(anima)
+    anima = setInterval(mover, 20, 1)
 })
 
 // window.onload=init()
 window.addEventListener('load', init())
-window.addEventListener('resize', redefinirTamMax)
