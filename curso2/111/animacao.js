3f3e2a8685fbcbcb3beb70fa1171d01ee1c79cfe
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-15 23:17:44 -0300

    terminei mais um exercicio sobre animação

--- curso2/111/animacao.js
+++ curso2/111/animacao.js
@@ -1,25 +1,26 @@
 const carro = document.getElementById('carro')
 const btn_parar = document.getElementById('btn_parar')
-
+const btn_rodar = document.getElementById('btn_rodar')
 
 let tamMax = window.innerWidth - parseInt(carro.style.width)
 let anima=null
+let tamCarro = null
 
 const init = ()=>{
     carro.style.position='relative'
     carro.style.left = '0px'
-    // carro.style.transition= '0.1s left'
     carro.style.width = '100px'
+    carro.style.height = '40px'
     redefinirTamMax()
 }
 
 const redefinirTamMax=()=>{
-    tamMax = window.innerWidth - parseInt(carro.style.width)
+    tamCarro = parseInt(carro.style.width)
+    tamMax = window.innerWidth - tamCarro
 }
 
 //* utilizando recursividade
 const mover=(dir)=>{
-    console.log(tamMax)
     let posicao_carro = parseInt(carro.style.left)
     if(posicao_carro > tamMax || posicao_carro < 0){
         dir *= -1
@@ -33,16 +34,21 @@ btn_parar.addEventListener('click', ()=>{
     clearTimeout(anima)
 })
 
-btn_esquerda.addEventListener('click', (evt)=>{
-    clearTimeout(anima)
-    mover(-1)
-})
-
-btn_direita.addEventListener('click', ()=>{
-    clearTimeout(anima)
+btn_rodar.addEventListener('click', ()=>{
     mover(1)
 })
 
 // window.onload=init()
 window.addEventListener('load', init())
 window.addEventListener('resize', redefinirTamMax)
+window.addEventListener('keydown', (evt)=>{
+    if( evt.code == 'ArrowUp'){
+        carro.style.width= parseInt(carro.style.width)+10+'px'
+        carro.style.height= parseInt(carro.style.height)+10+'px'
+    }
+    if( evt.code == 'ArrowDown'){
+        carro.style.width= parseInt(carro.style.width)-10+'px'
+        carro.style.height= parseInt(carro.style.height)-10+'px'
+    }
+    redefinirTamMax()
+})
