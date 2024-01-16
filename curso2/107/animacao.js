8ba640f9ab952b3e8b98d68fad73bb8823067258
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-15 21:53:00 -0300

    fiz uma animação

--- curso2/107/animacao.js
+++ curso2/107/animacao.js
@@ -1,4 +1,5 @@
 const carro = document.getElementById('carro')
+const btn_parar = document.getElementById('btn_parar')
 const btn_esquerda = document.getElementById('btn_esquerda')
 const btn_direita = document.getElementById('btn_direita')
 
@@ -8,28 +9,30 @@ const init = ()=>{
     carro.style.transition= '0.1s left'
 }
 
-const separarPrimeiroNumero = (termo)=>{
-    return /^\d+/.exec(termo)[0]
+const posicaoDentroDaTela = ()=>{
+    return parseInt(carro.style.left) >= 0 && parseInt(carro.style.left) < (document.body.offsetWidth - carro.offsetWidth)
 }
 
-const posicaoDentroDaTela = ()=>{
-    return carro.style.left <0 
+let anima=null
+
+const mover=(dir)=>{
+    let posicao_carro = parseInt(carro.style.left)
+    posicao_carro+= 10 * dir
+    carro.style.left = posicao_carro + 'px'
 }
+
+btn_parar.addEventListener('click', ()=>{
+    clearInterval(anima)
+})
 
 btn_esquerda.addEventListener('click', (evt)=>{
-    if(posicaoDentroDaTela()){
-        let posicao_carro = Number(separarPrimeiroNumero(carro.style.left))
-        posicao_carro -=10
-        carro.style.left = posicao_carro + 'px'
-        console.log(posicao_carro)
-    }
+    clearInterval(anima)
+    anima = setInterval(mover, 20, -1)
 })
 
 btn_direita.addEventListener('click', ()=>{
-    let posicao_carro = Number(separarPrimeiroNumero(carro.style.left))
-    posicao_carro +=10
-    carro.style.left = posicao_carro + 'px'
-    console.log(posicao_carro)
+    clearInterval(anima)
+    anima = setInterval(mover, 20, 1)
 })
 
 // window.onload=init()
