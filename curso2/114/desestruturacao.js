1d1ecb80fd4226e342a4d7e9eaddee1f9d819882
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-17 20:42:32 -0300

    aprendendo desestruturação

---
+++ curso2/114/desestruturacao.js
@@ -0,0 +1,26 @@
+// let numeros = [10,20,30,40]
+
+// // let[e,f,g,h]=numeros
+
+// let [e=0,f=0,g=0,h=0]=[10];
+
+// // ({a,b,c,d} = {a:'verde',b:'amarelo',c:'azul',d:'branco'})
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+
+let numeros=()=>{
+    return [10,20,30,40]
+}
+
+let [a,b,c,d] = numeros()
+
+// [a,b] = [b,a]
+
+console.log(a)
