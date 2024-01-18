38bdb79aab26bd4f37f6f8c8c7f80b4146ec4626
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-18 11:50:21 -0300

    aprendi validação

--- curso2/119/validacao.js
+++ curso2/119/validacao.js
@@ -4,12 +4,17 @@ const btn_validar = document.getElementById('btn_validar')
 const f_msg = document.getElementById('f_msg')
 
 btn_validar.addEventListener('click', (evt)=>{
-    let msg
-
-    if(!f_nota.checkValidity()){
-        msg = f_nota.validationMessage
+    let estadoValidacao=f_nota.validity
+    console.log(f_nota.validity)
+    if(estadoValidacao.valueMissing){
+        f_nota.setCustomValidity('Poxa, este campo é obrigatório')
+    }else if(estadoValidacao.rangeOverflow){
+        f_nota.setCustomValidity('Nossa, que nota alta você digitou, hein')
+    }else if(estadoValidacao.rangeUnderflow){
+        f_nota.setCustomValidity('Credo, que nota baixa!')
     }
 
-    f_msg.innerHTML = msg
+    f_nota.reportValidity()
+
     evt.preventDefault()
 })
