7c068bfbc6f3fc78108191d4eff7efcce1aaa5d1
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-21 16:36:16 -0300

    aprendi a fazer elementos reaproveitáveis

---
+++ curso2/133/topo.js
@@ -0,0 +1,30 @@
+// const head = document.head;
+const body = document.body;
+// const estilo='<link rel="stylesheet" href="topo.css">';
+// head.innerHTML+=estilo;
+
+const estiloTopo='display: flex;'+
+    'justify-content: space-between;'+
+    'align-items: center;'+
+    'background-color: blue;';
+
+const topo=document.createElement('div');
+topo.setAttribute('id', 'topo');
+topo.setAttribute('class', 'topo');
+topo.setAttribute('style', estiloTopo);
+
+body.prepend(topo);
+
+const estiloImg='width:200px;';
+const logo = '<div id="logo" class="logo">'+
+                '<img src="logo.jpg" title="CFBCursos" style="'+estiloImg+'">'+
+                '</div>';
+
+topo.innerHTML+=logo;
+
+const login = '<div id="login" class="login">'+
+                '<p id="matricula">Matrícula:<span></span></p>'+
+                '<p id="nome">Nome:<span></span></p>'+
+                '</div>';
+
+topo.innerHTML+=login;
