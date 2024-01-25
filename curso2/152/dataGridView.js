2688635dd27ef8273d5f2a0f9b5d00768b1a9933
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-01-25 16:43:49 -0300

    fazendo um dataGridView com acesso à banco de dados

--- curso2/152/dataGridView.js
+++ curso2/152/dataGridView.js
@@ -1,5 +1,5 @@
 const configDataGridView={
-    endpoint:'produtos.json',
+    endpoint:'http://127.0.0.1:1880/produtos',
     idDestino: 'dataGridViewDados'
 }
 
@@ -15,22 +15,22 @@ const dataGridView=(config)=>{
 
             const c1=document.createElement('div');
             c1.setAttribute('class', 'c1');
-            c1.innerHTML=el.id;
+            c1.innerHTML=el.n_id_produto;
             linha.appendChild(c1);
 
             const c2=document.createElement('div');
             c2.setAttribute('class', 'c2');
-            c2.innerHTML=el.produto;
+            c2.innerHTML=el.s_nome_produto;
             linha.appendChild(c2);
 
             const c3=document.createElement('div');
             c3.setAttribute('class', 'c3');
-            c3.innerHTML=el.marca;
+            c3.innerHTML=el.s_marca_produto;
             linha.appendChild(c3);
 
             const c4=document.createElement('div');
             c4.setAttribute('class', 'c4');
-            c4.innerHTML=el.modelo;
+            c4.innerHTML=el.s_modelo_produto;
             linha.appendChild(c4);
 
             const c5=document.createElement('div');
@@ -41,6 +41,16 @@ const dataGridView=(config)=>{
             const imgDelete = document.createElement('img');
             imgDelete.setAttribute('class', 'icone');
             imgDelete.setAttribute('src', 'delete.svg');
+            imgDelete.addEventListener('click', (evt)=>{
+                const id_produto = evt.target.parentNode.parentNode.firstElementChild.innerHTML;
+                const linha = evt.target.parentNode.parentNode;
+                linha.remove();
+                const endpoint='http://127.0.0.1:1880/removeproduto/'+id_produto;
+                fetch(endpoint)
+                .then(res=>{
+                    console.log(res);
+                })
+            })
             c5.appendChild(imgDelete);
 
             const imgEdit = document.createElement('img');
