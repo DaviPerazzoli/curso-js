9e6e976dc6f3a03198ffe5a908cf3717c7ca5800
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-02-02 16:02:15 -0300

    fazendo a pesquisa

--- curso2/161/pesquisar.js
+++ curso2/161/pesquisar.js
@@ -2,10 +2,21 @@ const btn_pesquisar = document.getElementById('btn_pesquisar');
 const f_txt_pesquisa = document.getElementById('f_txt_pesquisa');
 
 btn_pesquisar.addEventListener('click', (evt)=>{
-    if(f_txt_pesquisa.value==""){
+    const valorPesq = f_txt_pesquisa.value
+    if(valorPesq==""){
         alert('Digite a pesquisa');
         f_txt_pesquisa.focus();
         return;
     }
-    console.log(document.getElementsByName('f_pesq').value);
+    let f_pesq = document.querySelector('input[name=f_pesq]:checked').value;
+    if(f_pesq=='id'){
+        f_pesq='contato';
+    }
+    let endpoint=`http://127.0.0.1:1880/pesquisarcontatos/${f_pesq}/${valorPesq}`;
+    
+    fetch(endpoint)
+    .then(res=>res.json())
+    .then(res=>{
+        console.log(res);
+    });
 })
